177958a4a382286f603a566a6a158dcc1360c8bd
Daniela Vieira <danielavieira2024@gmail.com>
2021-10-24 22:28:55 -0300

    Add files via upload

---
+++ pagina02.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <title>Meu primeiro site em PHP</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <style type="text/css">
+        .linha
+        {
+            font-weight: bold;
+            color: red;
+            padding-left: 10px;
+            line-height: 50px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+        for ( $i = 0 ; $i < 10 ; $i++ )
+        {
+            print ( "<span class=\"Linha\">Linha número " . $i . "</span><br/>");
+        }
+    ?>
+</body>
+<script type="text/javascript">
+    $(document).ready
+    (function()
+        {
+            alert("wohoo!");
+        }
+    );
+</script>
+</html>
