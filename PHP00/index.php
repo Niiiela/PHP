2ab458be88c14f3a30c0567bdd1dac66675f665b
niiiela <danielavieira2024@gmail.com>
2023-10-23 17:55:43 -0300

    Impressão de dados com PHP

---
+++ PHP00/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Impressão em PHP</title>
+</head>
+<body>
+    <!--Impressão de dados em PHP-->
+    <?php
+        echo "<p>Exemplo 01: HTML e PHP</P>"
+    ?>
+    <!--Impressão de dados em PHP-->
+    <?= "<p>Exemplo 02: PHP e HTML</p>" ?>
+</body>
+</html>
